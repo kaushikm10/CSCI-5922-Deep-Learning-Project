075d557e4bda7e40e15e05ed1a8d34ef17f11ac1
kaushikm10 <kaushik.metha@somaiya.edu>
2023-12-08 04:03:28 -0700

    results added

--- main.py
+++ main.py
@@ -7,12 +7,26 @@ from PIL import Image
 import cv2
 import pandas as pd
 import io
+from skimage.metrics import peak_signal_noise_ratio, structural_similarity
+from tensorflow.keras import backend as K
+
+def psnr(y_true, y_pred):
+    y_true = K.clip(y_true, 0.0, 1.0)
+    y_pred = K.clip(y_pred, 0.0, 1.0)
+    mse = K.mean(K.square(y_true - y_pred))
+    max_pixel = 1.0
+    psnr = 20*K.log(max_pixel/K.sqrt(mse))/K.log(10.0)
+    return psnr
+
+def SSIM(y_true, y_pred):
+    return tf.reduce_mean(tf.image.ssim(y_true, y_pred, 1.0))
 
 app = Flask(__name__)
-model = tf.keras.models.load_model('models/model_1000_epochs_psnr_ssim.h5')
-df = pd.read_csv('csvs/model_1000_epochs.csv')
+model = tf.keras.models.load_model('models/model_1000_epochs_psnr_ssim.h5', custom_objects={'psnr': psnr, 'SSIM': SSIM})
+df = pd.read_csv('csvs/model_1000_epochs_psnr_ssim.csv')
 
 
+ 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -44,16 +58,19 @@ def data():
 
 @app.route('/results')
 def results():
-    return render_template('results.html')
+    loss = list(df['loss'].values)
+    val_loss = list(df['val_loss'].values)
+    ssim = list(df['SSIM'].values)
+    val_ssim = list(df['val_SSIM'].values)
+    psnr  = list(df['psnr'].values)
+    val_psnr = list(df['val_psnr'].values)
+    x_axis = list(range(len(loss)))
+    return render_template('results.html', x_axis=x_axis, loss=loss, val_loss=val_loss, ssim=ssim, val_ssim=val_ssim, psnr=psnr, val_psnr=val_psnr)
 
 @app.route('/analysis')
 def analysis():
-    loss = list(df['loss'].values)
-    val_loss = list(df['val_loss'].values)
-    acc = list(df['acc'].values)
-    val_acc = list(df['val_acc'].values)
-    x_axis = list(range(len(loss)))
-    return render_template('analysis.html', x_axis=x_axis, loss=loss, val_loss=val_loss, acc=acc, val_acc=val_acc)
+    return render_template('analysis.html')
+
 
 @app.route('/conclusion')
 def conclusion():
